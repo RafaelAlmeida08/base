c0757daec91aaad257c335a95c3922f5dd9e9103
Rafael Almeida <55145993+RafaelAlmeida08@users.noreply.github.com>
2020-08-14 12:16:06 -0300

    entidades

--- src/components/Pageselecao/index.js
+++ src/components/Pageselecao/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Card from '../Card'
 import Sidebar from '../Sidebar';
 import './pagselecao.css';
+import data from '../../jsonformatter.json';
 
 class Pageselecao extends Component{
 
@@ -23,11 +24,17 @@ class Pageselecao extends Component{
                     <div id="pageselecao-header">
                         <h1>Saúde</h1>                     
                         <hr/>
-                        <div id="search">
-                            <span>
-                                <img src={require('../images/search.svg')} alt="search"></img>
-                            </span>
-                            <input type="text" placeholder="Pesquisa..." aria-placeholder="Pesquisa"/>
+                        <div id="search">                           
+                            <select>
+                            {
+                                data.tbl_entidade.length > 0 ? 
+                                data.tbl_entidade.map((index) => 
+                                    <option value={index.codigo_codemp}>
+                                        {index.nome}
+                                    </option>
+                                    ) : <option>Nenhuma cidade cadastrada</option>
+                            }
+                            </select>
                         </div>
                     </div>
                     <Card clickedPlan={this.state.clickedPlan} telefone={this.props.telefone} />
