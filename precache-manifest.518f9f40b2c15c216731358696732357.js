e31b2283027b61d8d4609c9374999bf2a0c33a1d
Erivks <mordecai.es27@gmail.com>
2020-08-17 09:52:35 -0300

    Updates

---
+++ precache-manifest.518f9f40b2c15c216731358696732357.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e86f5d76a43ac935c5cff65ede906e",
+    "url": "/base/index.html"
+  },
+  {
+    "revision": "600e992843a4ac4c8d2f",
+    "url": "/base/static/css/main.a034d639.chunk.css"
+  },
+  {
+    "revision": "8acffc6fc782e52ed150",
+    "url": "/base/static/js/2.055ec0d8.chunk.js"
+  },
+  {
+    "revision": "e7db9ac6512e7fc159d3633db8f2050e",
+    "url": "/base/static/js/2.055ec0d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600e992843a4ac4c8d2f",
+    "url": "/base/static/js/main.a24b8ab5.chunk.js"
+  },
+  {
+    "revision": "d1dab1c12f5307f06e8e",
+    "url": "/base/static/js/runtime-main.1993568f.js"
+  },
+  {
+    "revision": "227eff6b3419929377f0a23bf4b0b647",
+    "url": "/base/static/media/compartilhar.227eff6b.svg"
+  },
+  {
+    "revision": "2a3c3fff37621ff938e35815763a4bf5",
+    "url": "/base/static/media/email.2a3c3fff.svg"
+  },
+  {
+    "revision": "92ddd7ca0ef929070a2d2712e4f70d38",
+    "url": "/base/static/media/imprimir.92ddd7ca.svg"
+  },
+  {
+    "revision": "5946d83944f03a453c1f614621c45757",
+    "url": "/base/static/media/logo_header.5946d839.png"
+  },
+  {
+    "revision": "59954327b73a8cac34e85eaa825eb503",
+    "url": "/base/static/media/mais.59954327.svg"
+  },
+  {
+    "revision": "badacf3ef59d81d8dfddeb107460f6c4",
+    "url": "/base/static/media/pdf.badacf3e.svg"
+  },
+  {
+    "revision": "1de0acef150f6502e9f80d4cbd618507",
+    "url": "/base/static/media/whatsapp.1de0acef.svg"
+  }
+]);
