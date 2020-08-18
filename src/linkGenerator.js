664a0bde42b0b145eba580670cd9c190500fa84b
Acran Laureano <acran.araujo@gmail.com>
2020-08-18 20:15:27 -0300

    linkGenerator

--- src/linkGenerator.js
+++ src/linkGenerator.js
@@ -16,7 +16,7 @@ async function linkGenerator (data) {
     
     let link = gerarLink(data)
 
-    const r = await fetch(`https://is.gd/create.php?format=json&url=${link}`)
+    const r = await fetch(`https://is.gd/create.php?format=json&url=${escape(link)}`)
     const l = await r.json()
     return l.shorturl
 
