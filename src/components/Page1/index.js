c6434e91a5eb8ab7349f75a5ad8b3059df3cad9e
Erivks <mordecai.es27@gmail.com>
2020-08-27 17:40:29 -0300

    Select de profissão

--- src/components/Page1/index.js
+++ src/components/Page1/index.js
@@ -128,21 +128,21 @@ class Page1 extends Component{
        
     }
 
-    setDataContato() {
-        var option = document.getElementById('select-estado').value;
-        var cidade = document.getElementById(option).attributes['data-praca'].value;
-        let dataContato = {
-            nome: document.getElementById('input-contato').value,
-            telefone: document.getElementById('input-telefone').value,
-            cidade: document.getElementById('select-estado').value,
-            consultor: document.getElementById('input-consultor').value,
-            telefone_consultor: document.getElementById('input-telefone-consultor').value,
-            celular_consultor: document.getElementById('input-celular-consultor').value,
-            email_consultor: document.getElementById('input-email').value,
-            praca: cidade
-        }
+    setDataContato() {            
+            var option = document.getElementById('select-estado').value;
+            var cidade = document.getElementById(option).attributes['data-praca'].value;
+            let dataContato = {
+                nome: document.getElementById('input-contato').value,
+                telefone: document.getElementById('input-telefone').value,
+                cidade: document.getElementById('select-estado').value,
+                consultor: document.getElementById('input-consultor').value,
+                telefone_consultor: document.getElementById('input-telefone-consultor').value,
+                celular_consultor: document.getElementById('input-celular-consultor').value,
+                email_consultor: document.getElementById('input-email').value,
+                praca: cidade
+            }
+            this.setState({contato: dataContato});
 
-        this.setState({contato: dataContato});
     }
 
     pagina2(e){
