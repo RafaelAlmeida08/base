07de473033e530f2abdc9ee07f37eb9391be984f
Erivks <mordecai.es27@gmail.com>
2020-08-28 17:22:31 -0300

    Deploy

---
+++ build/precache-manifest.46bc41ddfe6887ccfb736b62f89bc599.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13384694ce5a0fc28854a75bd68e7555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "011705e5757b10a6942d",
+    "url": "/static/css/main.12074210.chunk.css"
+  },
+  {
+    "revision": "780ffef8e9f71fd05b77",
+    "url": "/static/js/2.5b0775c9.chunk.js"
+  },
+  {
+    "revision": "e7db9ac6512e7fc159d3633db8f2050e",
+    "url": "/static/js/2.5b0775c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "011705e5757b10a6942d",
+    "url": "/static/js/main.da96b5c3.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  },
+  {
+    "revision": "227eff6b3419929377f0a23bf4b0b647",
+    "url": "/static/media/compartilhar.227eff6b.svg"
+  },
+  {
+    "revision": "2a3c3fff37621ff938e35815763a4bf5",
+    "url": "/static/media/email.2a3c3fff.svg"
+  },
+  {
+    "revision": "2e00a14089f99992d5b0b7a0cf09dd3d",
+    "url": "/static/media/familia.2e00a140.jpg"
+  },
+  {
+    "revision": "92ddd7ca0ef929070a2d2712e4f70d38",
+    "url": "/static/media/imprimir.92ddd7ca.svg"
+  },
+  {
+    "revision": "15977cf0ae64e2927d33de4ebf260d68",
+    "url": "/static/media/logo.15977cf0.png"
+  },
+  {
+    "revision": "5946d83944f03a453c1f614621c45757",
+    "url": "/static/media/logo_header.5946d839.png"
+  },
+  {
+    "revision": "59954327b73a8cac34e85eaa825eb503",
+    "url": "/static/media/mais.59954327.svg"
+  },
+  {
+    "revision": "e14cba5b6048a75a6bfdc2f27327c8b2",
+    "url": "/static/media/pdf-branco.e14cba5b.svg"
+  },
+  {
+    "revision": "badacf3ef59d81d8dfddeb107460f6c4",
+    "url": "/static/media/pdf.badacf3e.svg"
+  },
+  {
+    "revision": "a53e0c11a3781e4a9861c26139020b04",
+    "url": "/static/media/whatsapp-branco.a53e0c11.svg"
+  },
+  {
+    "revision": "1de0acef150f6502e9f80d4cbd618507",
+    "url": "/static/media/whatsapp.1de0acef.svg"
+  }
+]);
